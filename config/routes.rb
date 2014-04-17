a462cf54b8a511eb4e31183b98c323003c9fa722
vagrant <skwekamolfk@gmail.com>
2014-04-17 11:17:56 +0000

    auth layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Auth::Engine.routes.draw do
-  devise_for :admins, class_name: "Administration::Admin", module: :devise
+  devise_for :admins, class_name: "Administration::Admin", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, module: :devise
 
-  devise_for :users, class_name: "Member::User", module: :devise
+  devise_for :users, class_name: "Member::User", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, module: :devise
 end
